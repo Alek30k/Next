4456ebd8a7753e539f2b19df9ecd2d5223bc0578
Alek30k <alemank23k@gmail.com>
2023-08-25 17:08:42 -0300

    last commit

---
+++ app/posts/[id]/page.jsx
@@ -0,0 +1,20 @@
+async function loadPost(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+
+  return data;
+}
+
+const page = async ({ params }) => {
+  const post = await loadPost(params.id);
+  return (
+    <div>
+      <h1>
+        {post.id} {post.title}
+      </h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+export default page;
